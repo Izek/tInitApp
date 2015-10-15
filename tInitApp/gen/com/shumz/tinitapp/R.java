615e51a442ae73ef2c0d4a0381b037d201c72c41
Shum <shum@1s-MacBook-Pro-43.local>
2015-10-15 17:47:20 +0300

    0002. Initial Layout of 1st Activity implemented.

--- tInitApp/gen/com/shumz/tinitapp/R.java
+++ tInitApp/gen/com/shumz/tinitapp/R.java
@@ -13,10 +13,15 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int btn_start_next_act=0x7f050000;
+    }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int str_act_main_greetings=0x7f040001;
+        public static final int str_app_name=0x7f040000;
+        public static final int str_btn_start_next_act=0x7f040002;
     }
 }
